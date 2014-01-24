d70045850b778b7d1270aa95f25c7e453de67af6
Michael Sproul <micsproul@gmail.com>
2014-01-24 18:25:09 +1100

    Working project template (barebones)

--- Project.mk
+++ Project.mk
@@ -2,56 +2,42 @@
 CC = avr-gcc
 CXX = avr-g++
 
-PROJECT = Blink
-BOARD = atmega328
-LIBRARIES = Ethernet SPI
+PROJECT = {PROJECT}
+BOARD = {BOARD}
+LIBRARIES = # List of libraries to use (case sensitive)
 
-BOARD_FLAGS = $(shell imp boardflags $(BOARD))
-BOARD_FLAGS = -mmcu=atmega328p -DF_CPU-16000000UL -DARDUINO=105
-
+BOARD_C_FLAGS ?= $(shell imp get cflags $(BOARD))
 DEFAULT_CFLAGS = -Os -w -Wl,--gc-sections -ffunction-sections -fdata-sections
-CFLAGS = $(DEFAULT_CFLAGS)
+CFLAGS = $(BOARD_C_FLAGS) $(DEFAULT_CFLAGS)
 
+SRC_DIRS ?= $(shell imp get src $(LIBRARIES) --board $(BOARD))
+HEADER_INCLUDES ?= $(shell imp get src $(LIBRARIES) --board $(BOARD) -I)
+LIB_INCLUDES ?= $(shell imp get lib $(LIBRARIES) --board $(BOARD) -L -l)
 
+# Add the header folders to the virtual path
+VPATH = $(SRC_DIRS)
 
-ARDUINO_DIR = /usr/share/arduino
-ARDUINO_CORE = $(ARDUINO_DIR)/hardware/arduino/cores/arduino
-BOARD_VARIANT = standard
-ARDUINO_VARIANT = $(ARDUINO_DIR)/hardware/arduino/variants/$(BOARD_VARIANT)
-
-INCLUDES = -I $(ARDUINO_CORE) -I $(ARDUINO_CORE)/avr-libc -I $(ARDUINO_VARIANT)
-
-# Setup Arduino libraries (Ethernet, etc)
-LIBRARIES = $(shell ls -d $(ARDUINO_DIR)/libraries/* $(ARDUINO_DIR)/libraries/*/utility/)
-INCLUDES += -I $(shell echo $(LIBRARIES) | sed "s/ / -I /g")
-VPATH = $(LIBRARIES)
-
-# Define the directory where the core Arduino library resides
-LIBDIR = /home/michael/Programming/imp/
-
-
+OBJECTS = $(PROJECT).o
 
 $(PROJECT).hex: $(PROJECT).elf
 	avr-objcopy -O ihex $< $@
 
-OBJECTS = $(PROJECT).o Ethernet.o Dhcp.o SPI.o w5100.o EthernetServer.o EthernetClient.o Dns.o EthernetUdp.o socket.o \
-	  SD.o File.o Sd2Card.o SdFile.o SdVolume.o Useless.o
 
 $(PROJECT).elf: $(OBJECTS)
-	avr-gcc $(BOARD_FLAGS) -fdata-sections -w -Wl,--gc-sections -o $@ $^ -L $(LIBDIR) -larduino -lm
+	avr-gcc $(CFLAGS) -o $@ $^ $(LIB_INCLUDES)
 	rm *.o
 
 upload: $(PROJECT).hex
 	avrdude -c arduino -p m328p -b 57600 -U flash:w:$< -P/dev/ttyUSB0
 
 %.o: %.ino
-	$(CXX) -x c++ $(BOARD_FLAGS) $(CFLAGS) -c -o $@ $< $(INCLUDES)
+	$(CXX) -x c++ $(CFLAGS) -c -o $@ $< $(HEADER_INCLUDES)
 
 %.o: %.cpp
-	$(CXX) $(BOARD_FLAGS) $(CFLAGS) -c -o $@ $< $(INCLUDES)
+	$(CXX) $(CFLAGS) -c -o $@ $< $(HEADER_INCLUDES)
 
 %.o: %.c
-	$(CC) $(BOARD_FLAGS) $(CFLAGS) -c -o $@ $< $(INCLUDES)
+	$(CC) $(CFLAGS) -c -o $@ $< $(HEADER_INCLUDES)
 
 clean:
 	rm -f *.o *.hex *.elf
