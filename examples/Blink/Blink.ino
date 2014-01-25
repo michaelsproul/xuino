ff4bae04ae503cbe131bff3fb7fbbafec5705d82
Michael Sproul <micsproul@gmail.com>
2014-01-25 14:58:23 +1100

    Added imp init command.
    Just need to do uploading and serial communication.

---
+++ examples/Blink/Blink.ino
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+#define LED_PIN 13
+
+#define name(x) ((x == 1) ? "ON" : "OFF")
+
+int STATES[] = {1, 0, 1, 1, 0};
+int counter = 0;
+
+void setup()
+{
+	pinMode(LED_PIN, OUTPUT);
+	Serial.begin(9600);
+}
+
+void loop()
+{
+	digitalWrite(LED_PIN, STATES[counter]);
+	Serial.println(name(STATES[counter]));
+	counter = (counter + 1) % 5;
+	delay(1000);
+}
